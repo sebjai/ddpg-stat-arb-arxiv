142f5799d884a5990024774e11bb72e5f7f0d564
w-liam1 <liam.welsh@mail.utoronto.ca>
2024-01-15 20:29:11 -0500

    adding files
    Erroneously overwrote files.

--- main_DDPG.py
+++ main_DDPG.py
@@ -1,25 +1,20 @@
 # -*- coding: utf-8 -*-
 
-import os, sys
-sys.path.insert(0, os.path.abspath(".."))
-
 import matplotlib.pyplot as plt
 plt.style.use('paper.mplstyle')
 
-from offset import DDPG, offset_env
+from MR_env import MR_env
+from DDPG import DDPG
 
-# =============================================================================
-# import os
-# os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
-# =============================================================================
 #%%
-env = offset_env.offset_env(T=1/12, sigma=0.5, kappa=0.03, eta = 0.05, xi=0.1,
-                 c=0.25, S0=2.5, R=5, pen=2.5, N = 100)
+env = MR_env(S_0=1, kappa=1, sigma=0.2, theta=1,
+             dt=0.25, T = int(20), 
+             I_max=10, lambd=1)
 
-ddpg = DDPG.DDPG(env,
+ddpg = DDPG(env, I_max = 10,
             gamma = 0.999, 
-            lr=5e-4,
+            lr=1e-3,
             name="test" )
  
 #%%    
-ddpg.train(n_iter = 10000, n_plot=1000, n_iter_Q=5, n_iter_pi=5)
+ddpg.train(n_plot=200, n_iter_Q=5, n_iter_pi=5)
